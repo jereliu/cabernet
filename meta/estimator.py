f8eda389aa506c4c6801f0721ee7c8069dc582d6
Jeremiah Zhe Liu <zhel90@gmail.com>
2019-02-25 16:13:24 -0500

    initialize class templates

---
+++ meta/estimator.py
@@ -0,0 +1,52 @@
+"""Class template for models."""
+import collections
+
+from abc import ABC, abstractmethod
+
+import meta.model as meta_model
+
+# set up EstimatorOps container with defaults
+EstimatorOps = collections.namedtuple("EstimatorOps",
+                                      ["init", "train", "loss",
+                                       "save", "pred", "summary"])
+EstimatorOps.__new__.__defaults__ = (None,) * len(EstimatorOps._fields)
+
+
+class Estimator(ABC):
+    """Class template for model parameter/statistic estimators."""
+
+    def __init__(self, model):
+        """Initializer."""
+        self.graph = None
+        self.param = None
+        self.ops = None
+        self.model = model
+
+    @abstractmethod
+    def config(self):
+        """Sets up estimator graph."""
+        ...
+
+    @abstractmethod
+    def run(self, sess):
+        """Executes estimator graph in a given session.
+
+        Args:
+            sess: (tf.Session) A session to run graph in.
+        """
+        ...
+
+    @property
+    def model(self):
+        return self.__model
+
+    @model.setter
+    def model(self, new_model):
+        """Setter method to make model property immutable."""
+        if self.__model:
+            raise ValueError("model already defined and cannot be changed.")
+
+        if not isinstance(new_model, meta_model.Model):
+            raise ValueError("The input model is not a valid Model class.")
+
+        self.__model = new_model
---
+++ meta/model.py
@@ -0,0 +1,60 @@
+"""Class template for models."""
+import collections
+
+from abc import ABC, abstractmethod
+
+MODEL_PARAM_DEFAULT = None
+SAMPLE_PARAM_DEFAULT = None
+VI_PARAM_DEFAULT = {"loc": None, "scale": None}
+
+
+class Model(ABC):
+    """Class template for models"""
+
+    def __init__(self, param_name, sample_name):
+        """Initializer."""
+        # check model/parameter/sample names are given
+        if not self.model_name or not self.param_name or not self.sample_name:
+            raise ValueError("`model_name`/`param_name`/`sample_name` empty.")
+
+        # initialize parameter containers.
+        self.model_param = dict(zip(param_name,
+                                    [MODEL_PARAM_DEFAULT, ] * len(param_name)))
+        self.vi_param = dict(zip(param_name,
+                                 [VI_PARAM_DEFAULT, ] * len(param_name)))
+        self.sample_dict = dict(zip(sample_name,
+                                    [SAMPLE_PARAM_DEFAULT, ] * len(sample_name)))
+
+    @staticmethod
+    @abstractmethod
+    def definition():
+        """Sets up model definition and parameters."""
+        ...
+
+    @staticmethod
+    def likelihood(outcome_rv, outcome_value):
+        """Returns tensor of model likelihood.
+
+        Args:
+            outcome_rv: (ed.RandomVariable) A random variable representing model outcome.
+            outcome_value: (np.ndarray) Values of the training data.
+
+        Returns:
+            (tf.Tensor) A tf.Tensor representing likelihood values to be optimized.
+        """
+        return outcome_rv.distribution.log_prob(outcome_value)
+
+    @abstractmethod
+    def variational_family(self):
+        """Defines variational family and parameters."""
+        ...
+
+    @abstractmethod
+    def posterior_sample(self):
+        """Sample posterior distribution for training sample."""
+        ...
+
+    @abstractmethod
+    def predictive_sample(self):
+        """Samples new observations."""
+        ...
